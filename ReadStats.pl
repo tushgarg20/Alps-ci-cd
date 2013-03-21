6f69fb709e6eae729e6558559729249959fe150f
himanshu.govil <hgovil@rrci22110.rr.intel.com>
2013-03-21 10:57:00 -0600

    - Creating git repository for Gsim - ALPS.   Added first version of build alps script.   Added all the input files - cdyn, gc, scaling factors.   Also added ReadStats.pl

---
+++ ReadStats.pl
@@ -0,0 +1,327 @@
+#!/usr/bin/perl
+use strict;
+
+my @input;
+my @forml;
+my $csv;
+my $bad;
+my $out;
+my $err;
+my $dbg;
+
+while(@ARGV)
+{ my $a=shift;
+  if($a eq '-csv'){ $csv=1; next;}
+  if($a eq '-o'){ $out=shift; next;}
+  if($a eq '-e'){ $err=shift; next;}
+  if($a eq '-d'){ $dbg=1; next;}
+  if($a=~/^-/){ $bad=1; last;}
+  if(!scalar @input){ push @input, $a; next;}
+  push @forml, $a;
+}
+
+if($err ne '')
+{ open STDERR, ">$err" or die "Cannot open $err";
+}
+if($out ne '')
+{ open STDOUT, ">$out" or die "Cannot open $out";
+}
+
+$bad=1 if !scalar @input;
+$bad=1 if !scalar @forml;
+
+die "USAGE:\tReadStats.pl [options] <gsim.stat> <formula.txt> [<formula1.txt> [<formula2.txt> ...]] \noptions:\t-csv\t-- output in csv format\n" if $bad;
+
+my $fname=$input[0];
+my @list;
+my %var;
+my %unit;
+my %mac;
+my %bad;
+my %eq;
+my %eqfn; ### equation definition file
+my %eqln; ### equation definition line
+my %wc;
+my %wcfn; ### regex first use file
+my %wcln; ### regex first use line
+my %data;
+
+my $lineno=0;
+my $equation;
+
+###
+### Read formula files
+###
+foreach my $formula(@forml)
+{ open FILE,"<$formula" or die "Cannot open $formula\n";
+  $formula=$1 if $formula=~/[\/\\]([^\/\\]+)$/;
+  my $linecount=0;
+  foreach my $line(<FILE>)
+  { $linecount++;
+    chomp $line; $line=~s/#.*$//; $line=~s/^\s+//; $line=~s/\s+$//;
+    next if $line eq '';
+    $lineno=$linecount if $equation eq '';
+    $equation.=$line;
+    my $tmp=$equation;
+    while($tmp=~s/.*?('[^']+')//){ if($wcfn{$1} eq ''){ $wcfn{$1}=$formula; $wcln{$1}=$linecount;}}
+    next if $equation=~s/\\$//;
+    read_formula($equation,$formula,$lineno);
+    $equation='';
+  }
+  close FILE;
+}
+
+###
+### Check dependencies (topological sort + strongly connected components)
+###
+my %dep;
+my @vars=keys %var;
+foreach my $v(keys %var){ $dep{$v}={};}
+foreach my $v(keys %var)
+{ foreach my $d(keys %{$var{$v}})
+  { if($var{$d})
+    { $dep{$d}{$v}=1;
+      if($d eq $v)
+      { print STDERR "##### $eqfn{$v} line $eqln{$v} - Circular dependency:\t$v <- $d\n";
+        $bad{$v}=1;
+      }
+    }
+    else { delete $var{$v}{$d};}
+  }
+}
+my %dfs0; # node visited
+my %dfs1; # node done
+my $count=0;
+foreach my $v(@vars)
+{ next if $dfs0{$v};
+  $dfs0{$v}=1;
+  my @stack=($v);
+  while(scalar @stack)
+  { my $x=$stack[scalar @stack-1];
+    foreach my $d(keys %{$dep{$x}})
+    { delete $dep{$x}{$d} if $dfs0{$d};
+    }
+    if(scalar keys %{$dep{$x}})
+    { my $d=(keys %{$dep{$x}})[0];
+      $dfs0{$d}=1; push @stack,$d;
+    }
+    else
+    { $count++;
+      $dfs1{$x}=$count;
+      pop @stack;
+    }
+  }
+}
+@vars = sort {$dfs1{$b}<=>$dfs1{$a}} @vars;
+
+foreach my $v(keys %var)
+{ foreach my $d(keys %{$var{$v}}){ $dep{$v}{$d}=1;}
+}
+%dfs0={};
+foreach my $v(@vars)
+{ next if $dfs0{$v};
+  $dfs0{$v}=1;
+  my @stack=($v);
+  my @scc=($v);
+  while(scalar @stack)
+  { my $x=$stack[scalar @stack-1];
+    foreach my $d(keys %{$dep{$x}})
+    { delete $dep{$x}{$d} if $dfs0{$d};
+    }
+    if(scalar keys %{$dep{$x}})
+    { my $d=(keys %{$dep{$x}})[0];
+      $dfs0{$d}=1; push @stack,$d;
+      push @scc,$d;
+    }
+    else
+    { pop @stack;
+    }
+  }
+  next if 1==scalar @scc;
+  print STDERR "##### $eqfn{$v} line $eqln{$v} - Circular dependency:\t";
+  foreach my $d(@scc)
+  { $bad{$d}=1;
+    print STDERR "$d <- ";
+  }
+  print STDERR "$v\n";
+}
+
+###
+### Read stat file
+###
+open FILE,"<$fname" or die "Cannot open $fname\n";
+foreach my $line(<FILE>)
+{ chomp $line; $line=~s/#.*$//; $line=~s/^\s+//; $line=~s/\s+$//;
+  next unless $line=~/^(\S+)\s+(\S+)/;
+  my $st=$1; my $val=$2;
+  $data{$st}=$val;
+  foreach my $x(keys %wc)
+  { next unless $st=~/$wc{$x}/;
+    $data{$x}=$data{$x}.',' if $data{$x} ne '';
+    $data{$x}=$data{$x}.$data{$st};
+  }
+  if($var{$st})
+  { print STDERR "##### $eqfn{$st} line $eqln{$st} - Name conflict:\t$st\n";
+    $bad{$st}=1;
+  }
+}
+close FILE;
+
+foreach my $x(sort keys %wc)
+{ next if $data{$x};
+  print STDERR "##### $wcfn{$x} line $wcln{$x} - No matches found:\t$x\n";
+}
+
+###
+### Evaluate
+###
+foreach my $st(@vars)
+{ next if $bad{$st};
+  foreach my $d(keys %{$var{$st}})
+  { next unless $bad{$d};
+    print STDERR "##### $eqfn{$st} line $eqln{$st} - Broken dependency:\t$st <- $d\n";
+    $bad{$st}=1; last;
+  }
+  next if $bad{$st};
+  my $tmp;
+  my $expr;
+  $tmp=$eq{$st};
+  while($tmp=~s/(.*?)('[^']+')//)
+  { my $x=$2; $expr.=$1;
+    $expr.=$data{$x};
+  }
+  $tmp=$expr.$tmp; $expr='';
+  while($tmp=~s/(.*?)([\w\.]+)//)
+  { my $x=$2; $expr.=$1;
+    $expr.=($data{$x} eq '')?$x:$data{$x};
+  }
+  $expr.=$tmp;
+
+  my $val=eval($expr);
+  if($val eq '')
+  { print STDERR "##### $eqfn{$st} line $eqln{$st} - Cannot evaluate:\t$st = $eq{$st}\n";
+    $bad{$st}=1;
+    next;
+  }
+  $data{$st}=$val;
+}
+
+###
+### Output
+###
+foreach my $st(@list)
+{ next if $st=~/^\./ && !$dbg;
+  my $val=$data{$st};
+  next if $val eq '';
+  $val=sprintf("%.2f",$val) unless $val eq int($val);
+  if($csv)
+  { print "$st,$val\n";
+  }
+  else
+  { print "$st\t$val\n";
+  }
+}
+
+sub read_formula
+{ my $equation=shift;
+  my $file=shift;
+  my $line=shift;
+  my $st; my $eq; my $un;
+  if($equation=~/^(\S+)\s*\((\S*)\)\s*=\s*(.*)\s*$/)
+  { $st=$1; $un=$2; $eq=$3;
+  }
+  elsif($equation=~/^(\S+)\s*=\s*(.*)\s*$/)
+  { $st=$1; $eq=$2;
+  }
+  if($eq eq '' || !$st=~/^@\w+@$|^\.?[a-zA-Z_][\w\.]+$/)
+  { print STDERR "##### $file line $line - Incorrect syntax, line ignored:\t$equation\n";
+    return;
+  }
+  while($eq=~/^([^@]*)@([^@]*)@(.*)$/)
+  { if($mac{$2} eq '')
+    { print STDERR "##### $file line $line - Macro not defined, line ignored:\t$2\n";
+      return;
+    }
+    $eq=$1.$mac{$2}.$3;
+  }
+  if($st=~/^@([\w]+)@$/)
+  { $mac{$1}=$eq;
+  }
+  else ### if($st=~/^[\w\.]+$/)
+  { if($var{$st})
+    { print STDERR "##### $file line $line - Duplicated stat, line ignored:\t$st\n";
+    }
+    else
+    { $var{$st}={}; push @list,$st;
+      $eq{$st}=$eq; $unit{$st}=$un;
+      $eqfn{$st}=$file; $eqln{$st}=$line;
+      my $tmp=$eq;
+      while($tmp=~s/'([^']*)'/ /)
+      { $wc{"'$1'"}=$1;
+      }
+      while($tmp=~s/([\w\.]+)//)
+      { $var{$st}{$1}=1;
+      }
+    }
+  }
+}
+
+sub SUM
+{ my $z=0;
+  foreach my $x(@_){ $z+=$x if $x ne '';}
+  return $z;
+}
+
+sub MIN
+{ my $z;
+  foreach my $x(@_)
+  { next if $x eq '';
+    next unless $z eq '' || $x<$z;
+    $z=$x;
+  }
+  return $z;
+}
+
+sub MAX
+{ my $z;
+  foreach my $x(@_)
+  { next if $x eq '';
+    next unless $z eq '' || $x>$z;
+    $z=$x;
+  }
+  return $z;
+}
+
+sub COUNT
+{ my $z=0;
+  foreach my $x(@_){ $z++ if $x ne '';}
+  return $z;
+}
+
+sub AVG
+{ my $s;
+  my $n;
+  foreach my $x(@_)
+  { next if $x eq '';
+    $s+=$x; $n++;
+  }
+  return unless $n;
+  return $s/$n;
+}
+
+sub DEV
+{ my $s;
+  my $t;
+  my $n;
+  foreach my $x(@_)
+  { next if $x eq '';
+    $s+=$x; $t+=$x*$x; $n++;
+  }
+  return unless $n;
+  $s/=$n; $t/=$n;
+  return sqrt($t-$s*$s);
+}
+
+sub DIFF
+{ return $_[0] if 1 == scalar @1;
+}
---
+++ build_alps.py
@@ -0,0 +1,347 @@
+from optparse import OptionParser
+import yaml
+import re
+
+#############################
+# Command Line Arguments
+#############################
+##def files_callback(option,opt,value,parser):
+##    setattr(parser.values,option.dest,value.split(" "))
+
+parser = OptionParser()
+parser.add_option("-i","--input",dest="input_file",
+                  help="Input file containing path to all input files")
+parser.add_option("-r","--residency",dest="residency_file",
+                  help="Name of input Residency file")
+parser.add_option("-o","--output",dest="output_file",
+                  help="Name of output YAML file")
+parser.add_option("-a","--architecture",dest="dest_config",
+                  help="Specify target architecture. For e.g. Gen9LPCLient,Gen8SoC")
+
+(options,args) = parser.parse_args()
+##print options.input_file
+##print options.residency_file
+##print options.output_file
+##print options.dest_config
+
+#################################
+# Global Variables
+#################################
+I = {}
+C = {}
+cdyn_precedence = ['Gen7','Gen7.5','Gen8','Gen8SoC','Gen9LPClient','Gen9LPSoC','Gen10LP']
+new_gc = {}
+process_hash = {}
+voltage_hash = {}
+cdyn_cagr_hash = {'syn':{},'ebb':{}}
+stepping_hash = {}
+cfg = options.dest_config
+#path = []
+paths = []
+
+#################################
+# Subroutines
+#################################
+def get_data(line, separator):
+    res = line.split(separator)
+    i = 0
+    while(i < len(res)):
+        res[i] = res[i].strip()
+        i = i + 1
+    return res
+
+def dfs(adict, path=[]):
+    if(type(adict) is not dict):
+        path.append(adict)
+        paths.append(path + [])
+        path.pop()
+        return
+    for key in adict:
+        path.append(key)
+        dfs(adict[key],path)
+        if(path):
+            path.pop()
+    return
+
+def split_string(source, splitlist):
+    index = 0
+    flag = True
+    result = []
+    word = ""
+    while(index < len(source)):
+        char = source[index]
+        if char in splitlist:
+            if(word):
+                result.append(word)
+                word = ""
+            result.append(char)
+            char = ""
+
+        word = word + char
+        index = index + 1
+    if(word):
+        result.append(word)
+    return result
+
+def get_base_config(stat):
+    if(stat not in cdyn_hash):
+        print "No cdyn weight is available for " + stat
+        return None,None
+    i = cdyn_precedence.index(cfg)
+    while(i >= 0):
+        config = cdyn_precedence[i]
+        if(config in cdyn_hash[stat]):
+            if('B0' in cdyn_hash[stat][config]):
+                return config,'B0'
+            elif('A0' in cdyn_hash[stat][config]):
+                return config,'A0'
+            else:
+                return config, None
+        i = i-1
+    return None,None
+    
+def get_eff_cdyn(cluster,unit,stat):
+    base_cfg,stepping = get_base_config(stat)
+    if(base_cfg == None or stepping == None):
+        return 0
+    #print stat,base_cfg,stepping
+    base_cdyn = cdyn_hash[stat][base_cfg][stepping]['weight']
+    cdyn_type = cdyn_hash[stat][base_cfg][stepping]['type']
+    ref_gc = cdyn_hash[stat][base_cfg][stepping]['ref_gc']
+    process_sf = process_hash[base_cfg][cfg]
+    if(process_sf == 'NA'):
+        process_sf = 0
+    voltage_sf = voltage_hash[base_cfg][cfg]
+    if(voltage_sf == 'NA'):
+        voltage_sf = 0
+    stepping_sf = stepping_hash[base_cfg]['A0']['B0'] if stepping =='A0' else 1
+    cdyn_cagr_sf = cdyn_cagr_hash[cdyn_type][cluster][base_cfg][cfg]
+    instances = I[unit]
+    if(cdyn_type == 'syn'):
+        newproduct_gc = new_gc[cluster][unit][cfg]
+    else:
+        newproduct_gc = 1
+    gc_sf = newproduct_gc/ref_gc
+    eff_cdyn = base_cdyn*instances*gc_sf*process_sf*voltage_sf*stepping_sf*cdyn_cagr_sf
+        
+    #print base_cdyn, eff_cdyn
+    return eff_cdyn
+
+def eval_formula(alist):
+    result = 0
+    formula = alist[-1]
+    formula = "".join(formula.split())
+    power_stat = alist[-2]
+    cluster = alist[0]
+    unit = alist[1]
+    formula_data = split_string(formula,"+-/*()")
+    cdyn_vars = []
+    res_vars = []
+    i = 0
+    while(i < len(formula_data)):
+        if(formula_data[i] == 'R'):
+            formula_data[i] = 'R['+power_stat+']'
+            res_vars.append(formula_data[i])
+        elif(formula_data[i] == 'C'):
+            formula_data[i] = 'C['+power_stat+']'
+            cdyn_vars.append(formula_data[i])
+        elif(re.search(r'^R\[.*\]',formula_data[i])):
+            res_vars.append(formula_data[i])
+        elif(re.search(r'^C\[.*\]',formula_data[i])):
+            cdyn_vars.append(formula_data[i])
+        i = i+1
+
+    for elem in res_vars:
+        key = split_string(elem,"[]")[2]
+        if(key not in R):
+            print "Residency for " + key + " is not there!!"
+            return 0
+
+    for elem in cdyn_vars:
+        key = split_string(elem,"[]")[2]
+        C[key] = get_eff_cdyn(cluster,unit,key)
+
+    formula = "".join(formula_data)
+    formula = formula.replace("[","['")
+    formula = formula.replace("]","']")
+    result = eval(formula)
+    return result
+
+
+####################################
+# Parsing Build ALPS Config File
+####################################
+
+input_hash = {}
+infile = open(options.input_file,'r')
+for line in infile:
+    data = get_data(line,"=")
+    input_hash[data[0]] = data[1]
+
+#print input_hash
+
+##############################
+# Parsing Residency File
+##############################
+R = {}
+resfile = open(options.residency_file,'r')
+for line in resfile:
+    data = get_data(line,",")
+    test = data[0]
+    if(re.search(r'^num_.*',test)):
+        I[data[0].split("_")[1]] = float(data[1])
+    else:
+        R[data[0]] = float(data[1])
+resfile.close()
+#for key in R:
+    #print key + "," + R[key]
+#print I
+
+##############################
+# Parsing Cdyn File
+##############################
+cdyn_hash = {}
+cdyn_file = open(input_hash['Cdyn'],'r')
+first_line = cdyn_file.readline()
+for line in cdyn_file:
+    data = get_data(line,",")
+    if(data[0] not in cdyn_hash):
+        cdyn_hash[data[0]] = {}
+    if(data[1] not in cdyn_hash[data[0]]):
+        cdyn_hash[data[0]][data[1]] = {}
+    if(data[2] not in cdyn_hash[data[0]][data[1]]):
+        cdyn_hash[data[0]][data[1]][data[2]] = {}
+    cdyn_hash[data[0]][data[1]][data[2]]['weight'] = float(data[3])
+    cdyn_hash[data[0]][data[1]][data[2]]['type'] = data[4]
+    cdyn_hash[data[0]][data[1]][data[2]]['ref_gc'] = float(data[5])
+cdyn_file.close()
+#print cdyn_hash
+
+################################
+# Parsing Gate Count File
+################################
+gc_file = open(input_hash['GateCount'],'r')
+header_line = gc_file.readline()
+header_data = get_data(header_line,",")[2:]
+for line in gc_file:
+    data = get_data(line,",")
+    length = len(data)
+    if(data[1] not in new_gc):
+        new_gc[data[1]] = {}
+    if(data[0] not in new_gc[data[1]]):
+        new_gc[data[1]][data[0]] = {}
+    for i in range(2,length):
+        new_gc[data[1]][data[0]][header_data[i-2]] = float(data[i])
+        
+gc_file.close()
+#print new_gc
+
+################################
+# Parsing Scaling Factor Files
+################################
+# Process Scaling Factor
+process_file = open(input_hash['Process_Scaling_Factors'],'r')
+first_line = process_file.readline()
+for line in process_file:
+    data = get_data(line,",")
+    if(data[0] not in process_hash):
+        process_hash[data[0]] = {}
+    if(data[1] not in process_hash[data[0]]):
+        process_hash[data[0]][data[1]] = {}
+    process_hash[data[0]][data[1]] = float(data[2]) if data[2] != 'NA' else data[2]
+process_file.close()
+#print process_hash
+
+voltage_file = open(input_hash['Voltage_Scaling_Factors'],'r')
+first_line = voltage_file.readline()
+for line in voltage_file:
+    data = get_data(line,",")
+    if(data[0] not in voltage_hash):
+        voltage_hash[data[0]] = {}
+    if(data[1] not in voltage_hash[data[0]]):
+        voltage_hash[data[0]][data[1]] = {}
+    voltage_hash[data[0]][data[1]] = float(data[2]) if data[2] != 'NA' else data[2]
+voltage_file.close()
+#print voltage_hash
+
+syn_cdyn_cagr_file = open(input_hash['syn_cdyn_cagr'],'r')
+first_line = syn_cdyn_cagr_file.readline()
+for line in syn_cdyn_cagr_file:
+    data = get_data(line,",")
+    if(data[0] not in cdyn_cagr_hash['syn']):
+        cdyn_cagr_hash['syn'][data[0]] = {}
+    if(data[1] not in cdyn_cagr_hash['syn'][data[0]]):
+        cdyn_cagr_hash['syn'][data[0]][data[1]] = {}
+    cdyn_cagr_hash['syn'][data[0]][data[1]][data[2]] = float(data[3]) if data[3]!='NA' else data[3]
+syn_cdyn_cagr_file.close()
+#print cdyn_cagr_hash['syn']
+
+ebb_cdyn_cagr_file = open(input_hash['ebb_cdyn_cagr'],'r')
+first_line = ebb_cdyn_cagr_file.readline()
+for line in ebb_cdyn_cagr_file:
+    data = get_data(line,",")
+    if(data[0] not in cdyn_cagr_hash['ebb']):
+        cdyn_cagr_hash['ebb'][data[0]] = {}
+    if(data[1] not in cdyn_cagr_hash['ebb'][data[0]]):
+        cdyn_cagr_hash['ebb'][data[0]][data[1]] = {}
+    cdyn_cagr_hash['ebb'][data[0]][data[1]][data[2]] = float(data[3]) if data[3]!='NA' else data[3]
+ebb_cdyn_cagr_file.close()
+#print cdyn_cagr_hash['ebb']
+
+stepping_file = open(input_hash['cdyn_stepping'],'r')
+first_line = stepping_file.readline()
+for line in stepping_file:
+    data = get_data(line,",")
+    if(data[0] not in stepping_hash):
+        stepping_hash[data[0]] = {}
+    if(data[1] not in stepping_hash[data[0]]):
+        stepping_hash[data[0]][data[1]] = {}
+    stepping_hash[data[0]][data[1]][data[2]] = float(data[3]) if data[3]!='NA' else data[3]
+stepping_file.close()
+
+#############################
+# Parse ALPS Formula File
+#############################
+formula_files = get_data(input_hash['ALPS_formula_file'],",")
+for ff in formula_files:
+    f = open(ff,'r')
+    yaml_data = yaml.load(f)
+    f.close()
+    ##print yaml_data
+    dfs(yaml_data)
+
+output_list = paths + []
+output_yaml_data = {'GT':{}}
+overview_data = {'cdyn':{}}
+
+for path in output_list:
+    path[-1] = eval_formula(path)
+    d = output_yaml_data['GT']
+    i = 0
+    while(True):
+        if('cdyn' not in d):
+            d['cdyn'] = 0
+        d['cdyn'] += path[-1]
+        if(i == len(path)-2):
+            d[path[i]] = float('%.3f'%float(path[i+1]))
+            break
+        else:
+            if(path[i] in d):
+                d = d[path[i]]
+                i = i+1
+                continue
+            else:
+                d[path[i]] = {}
+        d = d[path[i]]
+        i = i+1
+
+#print output_yaml_data
+
+####################################
+# Generating output YAML file
+####################################
+of = open(options.output_file,'w')
+yaml.dump(output_yaml_data,of,default_flow_style=False)
+of.close()
+
+
