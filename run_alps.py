d638967d6c1a496bf4191904d1e0c8eb70f103bb
Dilip Somavarapu <dilip.somavarapu@intel.com>
2013-11-05 11:53:14 -0800

    added outputdir to input timegraph and output timegraph files in run_alps.py

--- run_alps.py
+++ run_alps.py
@@ -108,7 +108,7 @@ if not options.run_local:
     input_file = '/p/gat/tools/gsim_alps/' + cfg_data['ALPS Input File'][0]
     build_alps_cmd = ['/usr/intel/pkgs/python/3.1.2/bin/python', '/p/gat/tools/gsim_alps/build_alps.py', '-i', input_file, '-r', res, '-a', options.dest_config, '-o', yaml ]
     if(options.tg_file):
-        build_alps_cmd += ['-t',options.tg_file,'-z','alps_Timegraph.txt']
+        build_alps_cmd += ['-t', '%s/%s' % (options.output_dir, options.tg_file), '-z', '%s/alps_Timegraph.txt' % options.output_dir]
     if(options.run_debug):
         build_alps_cmd += ['--debug']
 
@@ -134,7 +134,7 @@ else:
     input_file = options.user_dir + '/' + cfg_data['ALPS Input File'][0]
     build_alps_cmd = ['/usr/intel/pkgs/python/3.1.2/bin/python', build_alps_script, '-i', input_file, '-r', res, '-a', options.dest_config, '-o', yaml ]
     if(options.tg_file):
-        build_alps_cmd += ['-t',options.tg_file,'-z','alps_Timegraph.txt']
+        build_alps_cmd += ['-t', '%s/%s' % (options.output_dir, options.tg_file), '-z', '%s/alps_Timegraph.txt' % options.output_dir]
     if(options.run_debug):
         build_alps_cmd += ['--debug']
 
