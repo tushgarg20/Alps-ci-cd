c5ec8f45b1dacdfdad21ba736ef1223a2529708f
Vasudha Chaudhary <vasudha.chaudhary@intel.com>
2015-04-13 15:30:31 -0700

    Enabled cnl alps_cfg_cnl.yaml file in the run_alps.py script      added idle BGF model in the fabric

--- run_alps.py
+++ run_alps.py
@@ -60,6 +60,8 @@ else:
         filename = '%s/alps_cfg_annealing.yaml' % wd
     elif options.dest_config.find('icl') > -1:
         filename = '%s/alps_cfg_icl.yaml' % wd
+    elif options.dest_config.find('cnl') > -1:
+        filename = '%s/alps_cfg_cnl.yaml' % wd
     else:	
         filename = '%s/alps_cfg.yaml' % wd
 
