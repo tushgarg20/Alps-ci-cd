c82aee9812eee38e30b1dfb6ecd7eecd7175ecf1
Arijit Mukhopadhyay <arijit.mukhopadhyay@intel.com>
2015-06-19 11:34:15 +0530

    Added the following changes:     1. Renamed the scripts/gen_top_power.newformat.pl script as scripts/gen_top_power.pl     2. Added the capability to parse the report into SMALL units as well as breakup SMALL units into CP, ASSIGN, RPT .

--- scripts/gen_top_power.pl
+++ scripts/gen_top_power.pl
@@ -9,6 +9,8 @@ gen_top_power.pl - Creates a partition DOP and top level glue/BUF power model mo
 	"help"			For printing help message 
 	"man" 			For printing detailed information
 	"debug"			For printing the sequence of commands
+	"small_part"		Partition the SMALL unit Cdyn into CP, ASSIGN, RPT
+	"par_list"		List of partitions (only required if --small_part is specified)
 	"gt_power_data"		Path to file containing idle/power virus data
 	"alps_unit_map"		Path to file unit to cluster ALPS-G mapping
 	"work_area"		Path to print output power data in a *.csv file
@@ -38,7 +40,9 @@ use YAML::XS qw(LoadFile);
 my $optHelp;
 my $optMan;
 my $debugMode;
+my $smallPart;
 
+my $parList;
 my $gtPowerData;
 my $alpsUnitMap;
 my $workArea;
@@ -51,6 +55,8 @@ Getopt::Long::GetOptions(
 	"help" => \$optHelp,
 	"man"  => \$optMan,   	
 	"debug" => \$debugMode,
+        "small_part" => \$smallPart,
+	"par_list=s" => \$parList,
 	"gt_power_data=s" => \$gtPowerData,
 	"alps_unit_map=s" => \$alpsUnitMap,
 	"work_area=s" => \$workArea,
@@ -59,6 +65,14 @@ Getopt::Long::GetOptions(
 
 pod2usage( -verbose => 1 ) if $optHelp;
 pod2usage( -verbose => 2 ) if $optMan;
+
+if ($smallPart) {print "Will partition SMALL Cdyn into CP|RPT|ASSIGN|REST\n";}
+
+if ($smallPart)
+{
+	if ($parList =~ /^$/) {die "Input partition list has not been provided\n";}
+	if (!(-f $parList)) {warn "Input partition list file $parList doesn't exist\n";}
+}
 
 if ($gtPowerData =~ /^$/) {die "Input GT power data file name not provided\n";}
 if (!(-f $gtPowerData)) {warn "Input file $gtPowerData doesn't exist\n"}
@@ -93,6 +107,23 @@ while(<$alpsUnitMapH>) {
 	}	
 }
 
+my %ipParH;
+
+if ($smallPart)
+{
+	my $ipParListH;
+	open $ipParListH, "$parList" or die "Cannot open input partition list $parList\n";
+
+	while (<$ipParListH>)
+	{
+		my $line = $_;
+		chomp($line);
+		my @parts = split(",", $line);
+		my $par = $parts[0];
+		$ipParH{"$par"} = 1;
+	}
+}
+
 close $alpsUnitMapH;
 
 my $gtPowerDataH;
@@ -103,6 +134,11 @@ my @gtPowerDataA;
 @gtPowerDataA = <$gtPowerDataH>;
 
 my %gtParH;
+my %gtCpParH;
+my %gtRptParH;
+my %gtAssignParH;
+
+my $parNameAssign = "";
 
 foreach (@gtPowerDataA) {
 	my $line = $_;
@@ -114,9 +150,26 @@ foreach (@gtPowerDataA) {
 	$line =~ s/^(,)+$//;
 	if ($line eq "") {next;}
 	my $parName;
+	my $isRptr = "";
 
 	my $parTop;
 	my $parDop;
+
+	my $parRpt;
+	my $parCpCmp;
+	my $parRptrCpCmp;
+	my $parCp;	
+	my $parAssign;
+
+	if ($smallPart)
+	{
+		my @parts = split(",", $line);
+		my $parLine = $parts[0];
+		if (exists $ipParH{"$parLine"})
+		{
+			$parNameAssign = $line;
+		}
+	}
 
 	my $parTopCell;
 	my $parTopDop;
@@ -126,6 +179,86 @@ foreach (@gtPowerDataA) {
 	my $parTopClkInv;
 	my $parTopClkCg;
 	my $parTopOther;
+
+        if ($smallPart)
+        {
+        	if ($line =~ /([a-zA-Z0-9_]+)_rpt_Power/)  {
+			$parName = $1;
+			my @parts = split(",", $line);
+			$parRpt = $parts[3];
+			$gtParH{$parName}{"RPT"} = $parRpt;
+		}
+
+		if ($line =~ /([a-zA-Z0-9_]+)(rptr)?cpunit1/)  {
+			$parName = $1;
+			if ($parName =~ /^(skl|cnl)/)
+			{
+				my $match = $1;
+				$parName =~ s/^$match//;
+			}
+			$isRptr  = $2;
+			my @parts = split(",", $line);
+			if (!(defined $isRptr))
+			{
+				$gtCpParH{$parName}{"CP"} = $parts[3];
+			}
+			elsif ($isRptr =~ /^rptr$/)
+			{
+				$gtCpParH{$parName}{"RPTRCP"} = $parts[3];
+			}
+			else
+			{
+				print "-WARN- $parName has a non-standrd RPTR pattern $isRptr\n";
+			}
+		}
+
+		if ($line =~ /([a-zA-Z0-9_]+)rptunit1/)  {
+			$parName = $1;
+			if ($parName =~ /_$/)
+			{
+				$parName =~ s/_$//;
+			}
+			if ($parName =~ /^(skl|cnl)/)
+			{
+				my $match = $1;
+				$parName =~ s/^$match//;
+			}
+			my @parts = split(",", $line);
+			$gtRptParH{$parName}{"RPT"} = $parts[3];
+		}
+
+		if ($line =~ /assign/)
+		{
+			if ($line !~/([a-zA-Z0-9_]+)_assign_Power/)
+			{
+				$parName = $parNameAssign;
+				my @parts = split(",", $line);
+				my $assignUnitName = $parts[0];
+				if ($parName !~ /^$/)
+				{
+					$gtAssignParH{$parName}{"ASSIGN"} += $parts[3];
+				}
+				else
+				{
+					print "-WARN- Couldn't find a valid partition name to add the assign unit $assignUnitName power to\n";
+				}
+			}
+		}
+
+		if ($line =~ /([a-zA-Z0-9_]+)_cp_Power/)  {
+			$parName = $1;
+			my @parts = split(",", $line);
+			$parCp = $parts[3];
+			$gtParH{$parName}{"CP"} = $parCp;
+		}
+
+		if ($line =~ /([a-zA-Z0-9_]+)_assign_Power/)  {
+			$parName = $1;
+			my @parts = split(",", $line);
+			$parAssign = $parts[3];
+			$gtParH{$parName}{"ASSIGN"} = $parAssign;
+		}
+	}
 
 	if ($line =~ /([a-zA-Z0-9_]+)_top_level_power/)  {
 		$parName = $1;
@@ -253,11 +386,90 @@ my $outputCsvFileH;
 $outputCsvFile = $workArea."/".$outputCsvFile;
 open $outputCsvFileH, ">$outputCsvFile" or die "Cannot open output CSV file: $outputCsvFile\n";
 
-print $outputCsvFileH "PAR, TOP, TOPCELL, TOPDOP, TOPBUF, TOPCLKDRIVER, TOPDELAYBUF, TOPCLKINV, TOPCLKCG, TOPOTHER, DOP, DFX, SMALL\n";
+if ($smallPart)
+{
+	print $outputCsvFileH "PAR, TOP, TOPCELL, TOPDOP, TOPBUF, TOPCLKDRIVER, TOPDELAYBUF, TOPCLKINV, TOPCLKCG, TOPOTHER, DOP, DFX, SMALL, CP, ASSIGN, RPT\n";
+}
+else
+{
+	print $outputCsvFileH "PAR, TOP, TOPCELL, TOPDOP, TOPBUF, TOPCLKDRIVER, TOPDELAYBUF, TOPCLKINV, TOPCLKCG, TOPOTHER, DOP, DFX, SMALL\n";
+}
 
 foreach my $key (keys %gtParH) {
 	#my $line = $key.",".$gtParH{$key}{"TOP"}.",".$gtParH{$key}{"DOP"}.",".$gtParH{$key}{"DFX"}.",".$gtParH{$key}{"SMALL"};
-	my $line = $key.",".$gtParH{$key}{"TOP"}.",".$gtParH{$key}{"TOPCELL"}.",".$gtParH{$key}{"TOPDOP"}.",".$gtParH{$key}{"TOPBUF"}.",".$gtParH{$key}{"TOPCLKDRIVER"}.",".$gtParH{$key}{"TOPDELAYBUF"}.",".$gtParH{$key}{"TOPCLKINV"}.",".$gtParH{$key}{"TOPCLKCG"}.",".$gtParH{$key}{"TOPOTHER"}.",".$gtParH{$key}{"DOP"}.",".$gtParH{$key}{"DFX"}.",".$gtParH{$key}{"SMALL"};
+	my $line;
+	if ($smallPart)
+	{
+		my $totCp;
+		my $totRpt;
+		my $totAssign;
+		if (exists $gtParH{$key}{"CP"})
+		{
+			$totCp = $gtParH{$key}{"CP"};
+		}
+		else
+		{
+			if (exists $gtCpParH{$key}{"CP"} && exists $gtCpParH{$key}{"RPTRCP"})
+			{
+				$totCp = $gtCpParH{$key}{"CP"} + $gtCpParH{$key}{"RPTRCP"};  
+			}
+			elsif (exists $gtCpParH{$key}{"CP"} && !(exists $gtCpParH{$key}{"RPTRCP"}))
+			{
+				$totCp = $gtCpParH{$key}{"CP"};
+			}
+			elsif (!(exists $gtCpParH{$key}{"CP"}) && exists $gtCpParH{$key}{"RPTRCP"})
+			{
+				$totCp = $gtCpParH{$key}{"RPTRCP"};
+			}
+			else
+			{
+				$totCp = 0;
+				print "-WARN- CP Unit power doesn't exist for partition $key\n";
+			}
+		}
+
+		if (exists $gtParH{$key}{"RPT"})
+		{
+			$totRpt = $gtParH{$key}{"RPT"};
+		}
+		else
+		{
+			if (exists $gtRptParH{$key}{"RPT"})
+			{
+				$totRpt = $gtRptParH{$key}{"RPT"};
+			}
+			else
+			{
+				$totRpt = 0;
+				print "-WARN- RPT Unit power doesn't exist for partition $key\n";
+			}
+		}
+
+		if (exists $gtParH{$key}{"ASSIGN"})
+		{
+			$totAssign = $gtParH{$key}{"ASSIGN"};
+		}
+		else
+		{
+			if (exists $gtAssignParH{$key}{"ASSIGN"})
+			{
+				$totAssign = $gtAssignParH{$key}{"ASSIGN"};
+			}
+			else
+			{
+				$totAssign = 0;
+				print "-WARN- ASSIGN Unit power doesn't exist for partition $key\n";
+			}
+		}
+
+		$line = $key.",".$gtParH{$key}{"TOP"}.",".$gtParH{$key}{"TOPCELL"}.",".$gtParH{$key}{"TOPDOP"}.",".$gtParH{$key}{"TOPBUF"}.",".$gtParH{$key}{"TOPCLKDRIVER"}.",".$gtParH{$key}{"TOPDELAYBUF"}.",".$gtParH{$key}{"TOPCLKINV"}.",".$gtParH{$key}{"TOPCLKCG"}.",".$gtParH{$key}{"TOPOTHER"}.",".$gtParH{$key}{"DOP"}.",".$gtParH{$key}{"DFX"}.",".$gtParH{$key}{"SMALL"}.",".$totCp.",".$totAssign.",".$totRpt;
+
+	}
+	else
+	{
+		$line = $key.",".$gtParH{$key}{"TOP"}.",".$gtParH{$key}{"TOPCELL"}.",".$gtParH{$key}{"TOPDOP"}.",".$gtParH{$key}{"TOPBUF"}.",".$gtParH{$key}{"TOPCLKDRIVER"}.",".$gtParH{$key}{"TOPDELAYBUF"}.",".$gtParH{$key}{"TOPCLKINV"}.",".$gtParH{$key}{"TOPCLKCG"}.",".$gtParH{$key}{"TOPOTHER"}.",".$gtParH{$key}{"DOP"}.",".$gtParH{$key}{"DFX"}.",".$gtParH{$key}{"SMALL"};
+	}
+
 	print $outputCsvFileH $line."\n"; 
 }
 
