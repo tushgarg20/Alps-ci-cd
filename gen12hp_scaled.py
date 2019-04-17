a8c27bdee44f86d9ef0f6e0a9cbb8387cc022e58
Sachin Kumar H S <sachin.kumar.h.s@intel.com>
2019-04-17 05:16:07 -0700

    Approximating GTI, SQIDI and Uncore cdyn in alps detailed modelled itself, instead of post processing script

--- gen12hp_scaled.py
+++ gen12hp_scaled.py
@@ -1,5 +1,6 @@
 import os, sys, re, subprocess, csv, itertools, parser, argparse, pdb, time, shlex
-import yaml
+#import yaml
+import lib.yaml as yaml 
 from subprocess import call
 from pathlib import Path
 
@@ -23,11 +24,7 @@ def alps_post_process(cdyn_dict,wl,lf,arch):
     unit_cdyn={}
     GT_cdyn={}
     #print (cdyn_dict,file=lf)
-    header_list = ['FPS','Total_GT_Cdyn(nF)','Total_GT_Cdyn_ebb(nF)','Total_GT_Cdyn_infra(nF)','Total_GT_Cdyn_syn(nF)']
-    for elements in header_list:
-        num=cdyn_dict[elements]
-        print ((elements+",\t").expandtabs(26)+(str(num)).expandtabs(16),file=lf)
-
+    
     print("##### Cluster level cdyn #####",file=lf)
     print ("",file=lf)
     cluster_cdyn={}
@@ -39,7 +36,7 @@ def alps_post_process(cdyn_dict,wl,lf,arch):
                 num = cdyn_dict['cluster_cdyn_numbers(pF)']["L3_Bank"]["total"] / 2.7
             elif clusters == "Fabric":
                 num = cdyn_dict['cluster_cdyn_numbers(pF)']["L3_Bank"]["total"] / 5.03'''
-            elif clusters == "SQIDI":
+            if clusters == "SQIDI":
                 num = cdyn_dict['cluster_cdyn_numbers(pF)']["GAM"]["total"] * 1.713
             elif clusters == "GTI":
                 num = (cdyn_dict['cluster_cdyn_numbers(pF)']["GAM"]["total"] * 0.0307) + 10.57
@@ -62,13 +59,13 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description='Script to post process the ALPS data and put it in s')
 
-    parser.add_argument('-o','--out_dir',dest="out_dir",default=False, help="Output directory")
+    #parser.add_argument('-o','--out_dir',dest="out_dir",default=False, help="Output directory")
     parser.add_argument('-i','--alps_dir',dest="alps_dir", help="ALPS directory")
     parser.add_argument('-a', '--arch', dest="arch", help="Architecture")
     args, sub_args = parser.parse_known_args()
    
     
-    timestr = time.strftime("%Y%m%d-%H%M%S")
+    '''timestr = time.strftime("%Y%m%d-%H%M%S")
     if args.out_dir:
         out_d = args.out_dir
         out_directory = "./"+out_d
@@ -79,15 +76,15 @@ if __name__ == '__main__':
     if os.path.isdir(out_directory):
         sys.exit("Error: Log directory already exists")
     else:
-        os.makedirs(out_directory)
+        os.makedirs(out_directory)'''
 
     if os.path.isdir(args.alps_dir):
 
         for files in os.listdir(args.alps_dir):
             if files.endswith(".yaml"):
                 wl = re.split(".yaml",files)
-                out_f = wl[0]+".csv"
-                lf = open(out_directory+"/"+out_f,'w')
+                out_f = wl[0]+"_post_processed_cdyn.csv"
+                lf = open(out_f,'w')
                 print ("###### Workload: "+wl[0]+"######",file=lf)
                 print ("", file=lf)
                 yaml_file = open(args.alps_dir+"/"+files,'r')
@@ -97,9 +94,9 @@ if __name__ == '__main__':
                             
     elif os.path.isfile(args.alps_dir):
         if args.alps_dir.endswith("yaml"):
-            wl = re.split(".yaml")
-            out_f = wl[0]+".csv"
-            lf = open(out_directory+"/"+out_f,'w')
+            wl = re.split(".yaml",files)
+            out_f = wl[0]+"_post_processed_cdyn.csv"
+            lf = open(out_f,'w')
             print ("###### Workload: "+wl[0]+"######",file=lf)
             print ("", file=lf)
             yaml_file = open(args.alps_dir+"/"+files,'r')
